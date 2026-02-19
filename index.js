2971ca8a30343338e8516840fcbf3d5953ce9932
geodome21 <gibson.h.matt@gmail.com>
2026-02-19 02:14:32 +0000

    ts is kinda cool i guess

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your local games are just HTML files in the root (or a folder)
 const games = [
   { title: "Deltarune", image: "images/Deltarune.png", file: "games/deltarune/index.html" },
-  { title: "Celeste", image: "images/celeste.png", file: "games/celeste/index.html" },
+  { title: "Hollow Knight", image: "images/hollow knight", file: "games/Hollow Knight/index.html" },
   { title: "Game 3", image: "images/game3.png", file: "game3.html" },
   { title: "Game 4", image: "images/game4.png", file: "game4.html" }
 ];
